4fec151f1bf553298d89f7d97ef3202170bd1f40
GandalfTGrey <GandalfTGrey@mmrr12138@gmail.com>
2023-09-17 21:42:43 +0800

    0

--- networks/pwc_decoder_ori.py
+++ networks/pwc_decoder_ori.py
@@ -20,7 +20,7 @@ from .correlation import corr_pwc as correlation
 from monodepth2.networks.ARFlow_models.correlation_native import Correlation as arflow_corr
 import numpy as np
 from monodepth2.utils.utils import torch_warp as flow_warp
-# def conv(in_planes, out_planes, kernel_size=3, stride=1, padding=1, dilation=1):   
+# def conv(in_planes, out_planes, kernel_size=3, stride=1 , dilation=1):   
 #     return nn.Sequential(
 #             nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, 
 #                         padding=padding, dilation=dilation, bias=True),
@@ -47,7 +47,7 @@ def conv(in_planes, out_planes, kernel_size=3, stride=1, dilation=1, isReLU=True
 def predict_flow(in_planes):
     return nn.Conv2d(in_planes,2,kernel_size=3,stride=1,padding=1,bias=True)
 
-def deconv(in_planes, out_planes, kernel_size=4, stride=2, padding=1):
+def deconv(in_planes, out_planes, kernel_size=4, stride=2, padding=1 ):
     return nn.ConvTranspose2d(in_planes, out_planes, kernel_size, stride, padding, bias=True)
 
 class sgu_model(nn.Module):
@@ -202,11 +202,11 @@ class PWCDecoder_from_img(nn.Module):
 
         self.leakyRELU = nn.LeakyReLU(0.1)
 
-        # self.conv_feat_scale1 = conv(64, 16, kernel_size=3, stride=1, padding=1)  # 1->2
-        # self.conv_feat_scale2 = conv(64, 32, kernel_size=3, stride=1, padding=1)  # 2->3
-        # self.conv_feat_scale3 = conv(128, 64, kernel_size=3, stride=1, padding=1)  # 3->4
-        # self.conv_feat_scale4 = conv(256, 96, kernel_size=3, stride=1, padding=1)  # 4->5
-        # self.conv_feat_scale5 = conv(512, 128, kernel_size=3, stride=1, padding=1)  # 5->6
+        # self.conv_feat_scale1 = conv(64, 16, kernel_size=3, stride=1 )  # 1->2
+        # self.conv_feat_scale2 = conv(64, 32, kernel_size=3, stride=1 )  # 2->3
+        # self.conv_feat_scale3 = conv(128, 64, kernel_size=3, stride=1 )  # 3->4
+        # self.conv_feat_scale4 = conv(256, 96, kernel_size=3, stride=1 )  # 4->5
+        # self.conv_feat_scale5 = conv(512, 128, kernel_size=3, stride=1 )  # 5->6
         # self.conv_scale5_to_scale_6 = conv(128, 196, kernel_size=3, stride=2)  # 6->5
         
 
@@ -221,8 +221,8 @@ class PWCDecoder_from_img(nn.Module):
         self.conv6_4 = conv(od+dd[3],32,  kernel_size=3, stride=1)        
         self.predict_flow6 = predict_flow(od+dd[4])
 
-        self.deconv6 = deconv(2, 2, kernel_size=4, stride=2, padding=1) 
-        self.upfeat6 = deconv(od+dd[4], 2, kernel_size=4, stride=2, padding=1) 
+        self.deconv6 = deconv(2, 2, kernel_size=4, stride=2 ) 
+        self.upfeat6 = deconv(od+dd[4], 2, kernel_size=4, stride=2 ) 
         
         od = nd+128+4
         self.conv5_0 = conv(od,      128, kernel_size=3, stride=1)
@@ -232,8 +232,8 @@ class PWCDecoder_from_img(nn.Module):
         self.conv5_4 = conv(od+dd[3],32,  kernel_size=3, stride=1)
         self.context_net5 = ContextNetwork(od+dd[4]+2)
         self.predict_flow5 = predict_flow(od+dd[4]) 
-        self.deconv5 = deconv(2, 2, kernel_size=4, stride=2, padding=1) 
-        self.upfeat5 = deconv(od+dd[4], 2, kernel_size=4, stride=2, padding=1) 
+        self.deconv5 = deconv(2, 2, kernel_size=4, stride=2 ) 
+        self.upfeat5 = deconv(od+dd[4], 2, kernel_size=4, stride=2 ) 
         
         od = nd+96+4
         self.conv4_0 = conv(od,      128, kernel_size=3, stride=1)
@@ -243,8 +243,8 @@ class PWCDecoder_from_img(nn.Module):
         self.conv4_4 = conv(od+dd[3],32,  kernel_size=3, stride=1)
         self.context_net4 = ContextNetwork(od+dd[4]+2)
         self.predict_flow4 = predict_flow(od+dd[4]) 
-        self.deconv4 = deconv(2, 2, kernel_size=4, stride=2, padding=1) 
-        self.upfeat4 = deconv(od+dd[4], 2, kernel_size=4, stride=2, padding=1) 
+        self.deconv4 = deconv(2, 2, kernel_size=4, stride=2 ) 
+        self.upfeat4 = deconv(od+dd[4], 2, kernel_size=4, stride=2 ) 
         
         od = nd+64+4
         self.conv3_0 = conv(od,      128, kernel_size=3, stride=1)
@@ -255,8 +255,8 @@ class PWCDecoder_from_img(nn.Module):
         self.context_net3 = ContextNetwork(od+dd[4]+2)
 
         self.predict_flow3 = predict_flow(od+dd[4]) 
-        self.deconv3 = deconv(2, 2, kernel_size=4, stride=2, padding=1) 
-        self.upfeat3 = deconv(od+dd[4], 2, kernel_size=4, stride=2, padding=1) 
+        self.deconv3 = deconv(2, 2, kernel_size=4, stride=2 ) 
+        self.upfeat3 = deconv(od+dd[4], 2, kernel_size=4, stride=2 ) 
         
         od = nd+32+4
         self.conv2_0 = conv(od,      128, kernel_size=3, stride=1)
@@ -266,8 +266,8 @@ class PWCDecoder_from_img(nn.Module):
         self.conv2_4 = conv(od+dd[3],32,  kernel_size=3, stride=1)
         self.context_net2 = ContextNetwork(od+dd[4]+2)
         self.predict_flow2 = predict_flow(od+dd[4]) 
-        self.deconv2 = deconv(2, 2, kernel_size=4, stride=2, padding=1)
-        self.upfeat2 = deconv(od+dd[4], 2, kernel_size=4, stride=2, padding=1)
+        self.deconv2 = deconv(2, 2, kernel_size=4, stride=2 )
+        self.upfeat2 = deconv(od+dd[4], 2, kernel_size=4, stride=2 )
         
         
         
@@ -279,8 +279,8 @@ class PWCDecoder_from_img(nn.Module):
         self.conv1_4 = conv(od+dd[3],32,  kernel_size=3, stride=1)
         self.context_net1 = ContextNetwork(od+dd[4]+2)
         self.predict_flow1 = predict_flow(od+dd[4])
-        self.up_feat1 = deconv(od+dd[4], 2, kernel_size=4, stride=2, padding=1)
-        self.deconv1 = deconv(2, 2, kernel_size=4, stride=2, padding=1)
+        self.up_feat1 = deconv(od+dd[4], 2, kernel_size=4, stride=2 )
+        self.deconv1 = deconv(2, 2, kernel_size=4, stride=2 )
         
         
         self.dc_conv1 = conv(od+dd[4], 128, kernel_size=3, stride=1,  dilation=1)
@@ -356,18 +356,18 @@ class PWCDecoder_from_img(nn.Module):
         
         c10 = im1
         c20 = im2
-        c11 = self.conv1b(self.conv1aa(self.conv1a(im1)))
-        c21 = self.conv1b(self.conv1aa(self.conv1a(im2)))
-        c12 = self.conv2b(self.conv2aa(self.conv2a(c11)))
-        c22 = self.conv2b(self.conv2aa(self.conv2a(c21)))
-        c13 = self.conv3b(self.conv3aa(self.conv3a(c12)))
-        c23 = self.conv3b(self.conv3aa(self.conv3a(c22)))
-        c14 = self.conv4b(self.conv4aa(self.conv4a(c13)))
-        c24 = self.conv4b(self.conv4aa(self.conv4a(c23)))
-        c15 = self.conv5b(self.conv5aa(self.conv5a(c14)))
-        c25 = self.conv5b(self.conv5aa(self.conv5a(c24)))
-        c16 = self.conv6b(self.conv6a(self.conv6aa(c15)))
-        c26 = self.conv6b(self.conv6a(self.conv6aa(c25)))
+        c11 = self.conv1b(self.conv1a(im1))
+        c21 = self.conv1b(self.conv1a(im2))
+        c12 = self.conv2b(self.conv2a(c11))
+        c22 = self.conv2b(self.conv2a(c21))
+        c13 = self.conv3b(self.conv3a(c12))
+        c23 = self.conv3b(self.conv3a(c22))
+        c14 = self.conv4b(self.conv4a(c13))
+        c24 = self.conv4b(self.conv4a(c23))
+        c15 = self.conv5b(self.conv5a(c14))
+        c25 = self.conv5b(self.conv5a(c24))
+        c16 = self.conv6b(self.conv6aa(c15))
+        c26 = self.conv6b(self.conv6aa(c25))
 
         # c11, c21 = self.conv_feat_scale1(resnet_feat1[0]), self.conv_feat_scale1(resnet_feat2[0])  # feature_1&2_level_1; (16, 96, 320), 1/2 scale
         # c12, c22 = self.conv_feat_scale2(resnet_feat1[1]), self.conv_feat_scale2(resnet_feat2[1])  # feature_1&2_level_2; (32, 48, 160), 1/4 scale
@@ -392,12 +392,13 @@ class PWCDecoder_from_img(nn.Module):
         # # up_flow6 = self.deconv6(flow6)
         x_intm, flow_res = self.flow_estimator(x)
         flow6 = flow_res + flow6
-        flow6 = flow6 + self.context_net(torch.cat((x_intm, flow6), dim=1))
+        flow_fine6 = self.context_net(torch.cat((x_intm, flow6), dim=1))
+        flow6 = flow6 + flow_fine6
         up_flow6 = F.interpolate(flow6 * 2, scale_factor=2, mode='bilinear', align_corners=True)
         # up_feat6 = self.upfeat6(x)
 
         
-        warp5 = self.warp_out(c25, up_flow6*0.625)
+        warp5 = self.warp_out(c25, up_flow6)
         corr5 = self.corr_block(c15, warp5) 
         corr5 = self.leakyRELU(corr5)
         
@@ -418,7 +419,7 @@ class PWCDecoder_from_img(nn.Module):
         # up_feat5 = self.upfeat5(x)
 
        
-        warp4 = self.warp_out(c24, up_flow5*1.25)
+        warp4 = self.warp_out(c24, up_flow5)
         corr4 = self.corr_block(c14, warp4)  
         corr4 = self.leakyRELU(corr4)
         x = torch.cat((corr4, self.conv_1x1[2](c14), up_flow5), 1)
@@ -426,7 +427,7 @@ class PWCDecoder_from_img(nn.Module):
         flow4 = flow_res + up_flow5
         flow4 = flow4 + self.context_net(torch.cat((x_intm, flow4), dim=1))
         
-        
+
         # x = torch.cat((corr4, c14, up_flow5), 1)
         # x = torch.cat((self.conv4_0(x), x),1)
         # x = torch.cat((self.conv4_1(x), x),1)
@@ -439,7 +440,7 @@ class PWCDecoder_from_img(nn.Module):
         # up_flow4 = self.deconv4(flow4)
         # up_feat4 = self.upfeat4(x)
 
-        warp3 = self.warp_out(c23, up_flow4*2.5)
+        warp3 = self.warp_out(c23, up_flow4)
         corr3 = self.corr_block(c13, warp3) 
         corr3 = self.leakyRELU(corr3)
         x = torch.cat((corr3, self.conv_1x1[3](c13), up_flow4), 1)
@@ -461,7 +462,7 @@ class PWCDecoder_from_img(nn.Module):
 
 
 
-        warp2 = self.warp_out(c22, up_flow3*5.0) 
+        warp2 = self.warp_out(c22, up_flow3)
         corr2 = self.corr_block(c12, warp2)
         corr2 = self.leakyRELU(corr2)
         x = torch.cat((corr2, self.conv_1x1[4](c12), up_flow3), 1)
@@ -568,11 +569,11 @@ class PWCDecoder(nn.Module):
 
         self.leakyRELU = nn.LeakyReLU(0.1)
 
-        self.conv_feat_scale1 = conv(64, 16, kernel_size=3, stride=1, padding=1)  # 1->2
-        self.conv_feat_scale2 = conv(64, 32, kernel_size=3, stride=1, padding=1)  # 2->3
-        self.conv_feat_scale3 = conv(128, 64, kernel_size=3, stride=1, padding=1)  # 3->4
-        self.conv_feat_scale4 = conv(256, 96, kernel_size=3, stride=1, padding=1)  # 4->5
-        self.conv_feat_scale5 = conv(512, 128, kernel_size=3, stride=1, padding=1)  # 5->6
+        self.conv_feat_scale1 = conv(64, 16, kernel_size=3, stride=1)  # 1->2
+        self.conv_feat_scale2 = conv(64, 32, kernel_size=3, stride=1)  # 2->3
+        self.conv_feat_scale3 = conv(128, 64, kernel_size=3, stride=1)  # 3->4
+        self.conv_feat_scale4 = conv(256, 96, kernel_size=3, stride=1)  # 4->5
+        self.conv_feat_scale5 = conv(512, 128, kernel_size=3, stride=1)  # 5->6
         self.conv_scale5_to_scale_6 = conv(128, 196, kernel_size=3, stride=2)  # 6->5
 
 
@@ -586,8 +587,8 @@ class PWCDecoder(nn.Module):
         self.conv6_3 = conv(od+dd[2],64,  kernel_size=3, stride=1)
         self.conv6_4 = conv(od+dd[3],32,  kernel_size=3, stride=1)        
         self.predict_flow6 = predict_flow(od+dd[4])
-        self.deconv6 = deconv(2, 2, kernel_size=4, stride=2, padding=1) 
-        self.upfeat6 = deconv(od+dd[4], 2, kernel_size=4, stride=2, padding=1) 
+        self.deconv6 = deconv(2, 2, kernel_size=4, stride=2) 
+        self.upfeat6 = deconv(od+dd[4], 2, kernel_size=4, stride=2) 
         
         od = nd+128+4
         self.conv5_0 = conv(od,      128, kernel_size=3, stride=1)
@@ -596,8 +597,8 @@ class PWCDecoder(nn.Module):
         self.conv5_3 = conv(od+dd[2],64,  kernel_size=3, stride=1)
         self.conv5_4 = conv(od+dd[3],32,  kernel_size=3, stride=1)
         self.predict_flow5 = predict_flow(od+dd[4]) 
-        self.deconv5 = deconv(2, 2, kernel_size=4, stride=2, padding=1) 
-        self.upfeat5 = deconv(od+dd[4], 2, kernel_size=4, stride=2, padding=1) 
+        self.deconv5 = deconv(2, 2, kernel_size=4, stride=2 ) 
+        self.upfeat5 = deconv(od+dd[4], 2, kernel_size=4, stride=2 ) 
         
         od = nd+96+4
         self.conv4_0 = conv(od,      128, kernel_size=3, stride=1)
@@ -606,8 +607,8 @@ class PWCDecoder(nn.Module):
         self.conv4_3 = conv(od+dd[2],64,  kernel_size=3, stride=1)
         self.conv4_4 = conv(od+dd[3],32,  kernel_size=3, stride=1)
         self.predict_flow4 = predict_flow(od+dd[4]) 
-        self.deconv4 = deconv(2, 2, kernel_size=4, stride=2, padding=1) 
-        self.upfeat4 = deconv(od+dd[4], 2, kernel_size=4, stride=2, padding=1) 
+        self.deconv4 = deconv(2, 2, kernel_size=4, stride=2 ) 
+        self.upfeat4 = deconv(od+dd[4], 2, kernel_size=4, stride=2 ) 
         
         od = nd+64+4
         self.conv3_0 = conv(od,      128, kernel_size=3, stride=1)
@@ -616,8 +617,8 @@ class PWCDecoder(nn.Module):
         self.conv3_3 = conv(od+dd[2],64,  kernel_size=3, stride=1)
         self.conv3_4 = conv(od+dd[3],32,  kernel_size=3, stride=1)
         self.predict_flow3 = predict_flow(od+dd[4]) 
-        self.deconv3 = deconv(2, 2, kernel_size=4, stride=2, padding=1) 
-        self.upfeat3 = deconv(od+dd[4], 2, kernel_size=4, stride=2, padding=1) 
+        self.deconv3 = deconv(2, 2, kernel_size=4, stride=2 ) 
+        self.upfeat3 = deconv(od+dd[4], 2, kernel_size=4, stride=2 ) 
         
         od = nd+32+4
         self.conv2_0 = conv(od,      128, kernel_size=3, stride=1)
@@ -626,18 +627,18 @@ class PWCDecoder(nn.Module):
         self.conv2_3 = conv(od+dd[2],64,  kernel_size=3, stride=1)
         self.conv2_4 = conv(od+dd[3],32,  kernel_size=3, stride=1)
         self.predict_flow2 = predict_flow(od+dd[4]) 
-        self.deconv2 = deconv(2, 2, kernel_size=4, stride=2, padding=1) 
+        self.deconv2 = deconv(2, 2, kernel_size=4, stride=2 ) 
         
-        self.dc_conv1 = conv(od+dd[4], 128, kernel_size=3, stride=1, padding=1,  dilation=1)
-        self.dc_conv2 = conv(128,      128, kernel_size=3, stride=1, padding=2,  dilation=2)
-        self.dc_conv3 = conv(128,      128, kernel_size=3, stride=1, padding=4,  dilation=4)
-        self.dc_conv4 = conv(128,      96,  kernel_size=3, stride=1, padding=8,  dilation=8)
-        self.dc_conv5 = conv(96,       64,  kernel_size=3, stride=1, padding=16, dilation=16)
-        self.dc_conv6 = conv(64,       32,  kernel_size=3, stride=1, padding=1,  dilation=1)
+        self.dc_conv1 = conv(od+dd[4], 128, kernel_size=3, stride=1, dilation=1)
+        self.dc_conv2 = conv(128,      128, kernel_size=3, stride=1, dilation=2)
+        self.dc_conv3 = conv(128,      128, kernel_size=3, stride=1, dilation=4)
+        self.dc_conv4 = conv(128,      96,  kernel_size=3, stride=1, dilation=8)
+        self.dc_conv5 = conv(96,       64,  kernel_size=3, stride=1, dilation=16)
+        self.dc_conv6 = conv(64,       32,  kernel_size=3, stride=1, dilation=1)
         self.dc_conv7 = predict_flow(32)
 
-        self.pred_flow_up1 = conv(in_planes=2, out_planes=16, kernel_size=3, stride=1, padding=1)
-        self.pred_flow_up2 = conv(in_planes=16, out_planes=2, kernel_size=3, stride=1, padding=1)
+        self.pred_flow_up1 = conv(in_planes=2, out_planes=16, kernel_size=3, stride=1 )
+        self.pred_flow_up2 = conv(in_planes=16, out_planes=2, kernel_size=3, stride=1 )
         
         for m in self.modules():
             if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
@@ -718,7 +719,7 @@ class PWCDecoder(nn.Module):
         up_feat6 = self.upfeat6(x)
 
         
-        warp5 = self.warp_out(c25, up_flow6*0.625)
+        warp5 = self.warp_out(c25, up_flow6)
         corr5 = self.corr_block(c15, warp5) 
         corr5 = self.leakyRELU(corr5)
         x = torch.cat((corr5, c15, up_flow6, up_feat6), 1)
@@ -732,7 +733,7 @@ class PWCDecoder(nn.Module):
         up_feat5 = self.upfeat5(x)
 
        
-        warp4 = self.warp_out(c24, up_flow5*1.25)
+        warp4 = self.warp_out(c24, up_flow5)
         corr4 = self.corr_block(c14, warp4)  
         corr4 = self.leakyRELU(corr4)
         x = torch.cat((corr4, c14, up_flow5, up_feat5), 1)
@@ -746,7 +747,7 @@ class PWCDecoder(nn.Module):
         up_feat4 = self.upfeat4(x)
 
 
-        warp3 = self.warp_out(c23, up_flow4*2.5)
+        warp3 = self.warp_out(c23, up_flow4)
         corr3 = self.corr_block(c13, warp3) 
         corr3 = self.leakyRELU(corr3)
         
@@ -762,7 +763,7 @@ class PWCDecoder(nn.Module):
         up_feat3 = self.upfeat3(x)
 
 
-        warp2 = self.warp_out(c22, up_flow3*5.0) 
+        warp2 = self.warp_out(c22, up_flow3) 
         corr2 = self.corr_block(c12, warp2)
         corr2 = self.leakyRELU(corr2)
         x = torch.cat((corr2, c12, up_flow3, up_feat3), 1)
@@ -775,11 +776,20 @@ class PWCDecoder(nn.Module):
  
         x = self.dc_conv4(self.dc_conv3(self.dc_conv2(self.dc_conv1(x))))
         flow2 = flow2 + self.dc_conv7(self.dc_conv6(self.dc_conv5(x)))
-        flow2 = self.upsample_conv(flow2)
-        flow3 = self.upsample_conv(flow3)
-        flow4 = self.upsample_conv(flow4)
-        flow5 = self.upsample_conv(flow5)
-        flow6 = self.upsample_conv(flow6)
+        # flow2 = self.upsample_conv(flow2)
+        # flow3 = self.upsample_conv(flow3)
+        # flow4 = self.upsample_conv(flow4)
+        # flow5 = self.upsample_conv(flow5)
+        # flow6 = self.upsample_conv(flow6)
+        
+        
+        
+        flow2 = F.interpolate(flow2 * 4, scale_factor=4, mode='bilinear', align_corners=True)
+        flow3 = F.interpolate(flow3 * 4, scale_factor=4, mode='bilinear', align_corners=True)
+        flow4 = F.interpolate(flow4 * 4, scale_factor=4, mode='bilinear', align_corners=True)
+        flow5 = F.interpolate(flow5 * 4, scale_factor=4, mode='bilinear', align_corners=True)
+        flow6 = F.interpolate(flow6 * 4, scale_factor=4, mode='bilinear', align_corners=True)
+        
         out = {'level0':flow2, 'level1':flow3, 'level2':flow4, 'level3':flow5, 'level4':flow6}
         return out
     
